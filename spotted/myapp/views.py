638166426d5ed2bc4e19e4859bf71b7b34704014
lucas le moine <lucas.le-moine@etu.utc.fr>
2021-11-13 17:55:04 +0100

    "Bootstrap_1"

--- spotted/myapp/views.py
+++ spotted/myapp/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
     return HttpResponse("Hello World")
+
+
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    #return HttpResponse(f"Book: {book.title}, published on {book.pub_date}")
+    return render(request, 'book.html', {'book': book})
+
+def profile(request):
+    return render(request, 'profile.html')
